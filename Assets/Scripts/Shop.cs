f987355eeda866b56bbd892a90269d2411c28684
Nakes1337 <aaaa-aaaabbbb-bbbb@mail.ru>
2022-12-03 09:52:46 +0300

    VNATURI GEGA(Shop restored)
    Shop restored

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -10,8 +10,9 @@ public class Shop : MonoBehaviour
     // Start is called before the first frame update
     public int money;
     public int total_money;
-    public int multiplayer; // miltiplayer buffs
-    public int buffs; //buffs
+    public int[] multiplayer = {1, 1, 1, 1, 1}; // miltiplayer buffs
+    public int[] intbuffs = new int[5]; 
+    public string buffs; //buffs
     public int[] prises = { 100, 200, 500, 700, 1000 }; //shop prises
     public Text MoneyText;
 
@@ -26,11 +27,23 @@ public class Shop : MonoBehaviour
     {
         money = PlayerPrefs.GetInt("money");
         total_money = PlayerPrefs.GetInt("total_money");
-        multiplayer = PlayerPrefs.GetInt("multiplayer");
-        buffs = PlayerPrefs.GetInt("buffs");
+        if (PlayerPrefs.GetString("buffs") == "0") buffs = "0,0,0,0,0";
+        else buffs = PlayerPrefs.GetString("buffs");
+        intbuffs = StringToArray(buffs);
 
         _group = GetComponent<VerticalLayoutGroup>();
         setBuffs();
+        StartCoroutine(IdleFarm());
+    }
+    int[] StringToArray(string teamp_line)
+    {
+        int[] temp_arr_int = new int[5];
+        string[] temp_arr_string = teamp_line.Split(',');
+        for (int i = 0; i < temp_arr_string.Length; i++)
+        {
+            temp_arr_int[i] = System.Int32.Parse(temp_arr_string[i].ToString());
+        }
+        return temp_arr_int;
     }
     void setBuffs()
     {
@@ -68,59 +81,66 @@ public class Shop : MonoBehaviour
     }
     void GetBuff(int id)
     {
-
+        string temp_collab = "";
         switch (id)
         {
             case 0:
-                if (money >= 100)
+                if (money >= prises[id])
                 {
-                    money -= 100;
-                    buffs++;
+                    money -= prises[id];
+                    intbuffs[id]++;
                     PlayerPrefs.SetInt("money", money);  
-                }
-                else
-                {
-
                 }
                 break;
             case 1:
-                if (money >= 200)
+                if (money >= prises[id])
                 {
-                    money -= 200;
-                    buffs += 2;
+                    money -= prises[id];
+                    intbuffs[id]++;
                     PlayerPrefs.SetInt("money", money);
                 }
                 break;
             case 2:
-                if (money >= 500)
+                if (money >= prises[id])
                 {
-                    money -= 500;
-                    buffs += 5;
+                    money -= prises[id];
+                    intbuffs[id]++;
                     PlayerPrefs.SetInt("money", money);
                 } 
                 break;
             case 3:
-                if (money >= 700)
+                if (money >= prises[id])
                 {
-                    money -= 700;
-                    buffs += 7;
+                    money -= prises[id];
+                    intbuffs[id]++;
                     PlayerPrefs.SetInt("money", money);
                 }
                 break;
             case 4:
-                if (money >= 1000)
+                if (money >= prises[id])
                 {
-                    money -= 1000;
-                    buffs += 10;
+                    money -= prises[id];
+                    intbuffs[id]++;
                     PlayerPrefs.SetInt("money", money);
                 }
                 break;
         }
+        for(int j = 0; j < intbuffs.Length; j++)
+        {
+            temp_collab = temp_collab.Insert(temp_collab.Length, intbuffs[j].ToString());
+            temp_collab += ",";
+        }
+        temp_collab = temp_collab.Remove(temp_collab.Length - 1, 1);
+        buffs = temp_collab;
+        PlayerPrefs.SetString("buffs", buffs);
     }
     IEnumerator IdleFarm()
     {
         yield return new WaitForSeconds(1);
-        money += buffs * multiplayer;
+        for (int i = 0; i < intbuffs.Length; i++)
+        {
+            money += intbuffs[i] * multiplayer[i];
+        }
         //Debug.Log(money);
         PlayerPrefs.SetInt("money", money);
         StartCoroutine(IdleFarm());
